3901567581f8f29329d96d763642dbefed488d1a
Stefan Stojkovic <stefan.test89@gmail.com>
2023-03-12 23:29:34 +0100

    Update 1

--- JWD59FinalTest(Svetsko prvenstvo)/jwd-back/src/main/java/ftni/jwd59/Zavrsni/repository/UtakmicaRepository.java
+++ JWD59FinalTest(Svetsko prvenstvo)/jwd-back/src/main/java/ftni/jwd59/Zavrsni/repository/UtakmicaRepository.java
@@ -1,7 +1,6 @@
 package ftni.jwd59.Zavrsni.repository;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
